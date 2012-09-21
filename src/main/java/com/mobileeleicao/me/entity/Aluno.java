501b37793e14c8bf7e2ca94e0b02627507f9331b
Simao Menezes <simaomenezesneto@gmail.com>
2012-09-20 23:33:56 -0300

    Criacao do pacote entity e de algumas entity

---
+++ src/main/java/com/mobileeleicao/me/entity/Aluno.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.mobileeleicao.me.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author simao
+ *
+ * Class que representa um aluno
+ */
+public class Aluno implements Serializable{
+	
+	private static final long serialVersionUID = -3378164210002817874L;
+	private Long id;
+	private String name;
+	private int matricula;
+	
+}
---
+++ src/main/java/com/mobileeleicao/me/entity/Equipe.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+package com.mobileeleicao.me.entity;
+
+import java.io.Serializable;
+
+/**
+ * @author simao
+ *
+ * Class que representa uma equipe
+ */
+public class Equipe implements Serializable{
+	 /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1741235279845592267L;
+	private Long id;
+	 private String name;
+	 private Aluno aluno;
+
+}
