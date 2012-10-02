06b35d0a3c8cd91e041e683490f39f061e7e79e8
Simao Menezes <simaomenezesneto@gmail.com>
2012-10-02 02:16:15 -0300

    Criacao da interface do servico para a entidade Aluno.

---
+++ src/main/java/com/mobileenquete/me/server/dao/DaoAluno.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.mobileenquete.me.server.dao;
+
+import java.io.Serializable;
+
+import org.springframework.stereotype.Component;
+
+import com.mobileenquete.me.entity.Aluno;
+
+/**
+ * @author simao
+ *
+ */
+@Component
+public class DaoAluno extends DaoAbstract<Aluno, Long>{
+	
+	private static final long serialVersionUID = 7208296872904579849L;
+
+	@Override
+	protected Long getId(Aluno aluno) {
+		// TODO Auto-generated method stub
+		return aluno.getId();
+	}
+
+}
